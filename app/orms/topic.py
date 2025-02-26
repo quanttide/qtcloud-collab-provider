4d3557a8f7d33f5ca2e51f96986d9e4c4b746fbd
Guo Zhang <iGuo19961110@gmail.com>
2025-02-26 19:58:58 +0800

    refactor: reorganize project

--- app/orms/topic.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from ..database import Base
-
-class Topic(Base):
-    __tablename__ = "topics"
-
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, unique=True, index=True)
-    description = Column(String, index=True)
